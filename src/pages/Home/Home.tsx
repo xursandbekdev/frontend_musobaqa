a04cc57368c0b19c7f7d334bd1f066f2bb36ff0e
kdrv dev <ffeodalik@gmail.com>
2025-06-01 14:38:38 +0500

    add:sidebar

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,79 @@
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+
+import { Button, Layout, Menu, theme } from "antd";
+import { useState } from "react";
+
+const { Header, Sider, Content } = Layout;
 
 const Home = () => {
-  return (
-    <div className=" underline text-red" >Home</div>
-  )
-}
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
 
-export default Home
+  return (
+    <Layout>
+      <Sider className="min-h-screen" trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" >
+
+        </div>
+        <Menu
+          theme="dark"
+          mode="inline"
+          className="mt-8"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "nav 1",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "nav 2",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "nav 3",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}>
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Home;
