972742b3ae7ffa57ba27219346f1a27964bcc0fa
to'raboyev xursand <xursandtoraboyev810@gmail.com>
2025-06-01 12:15:08 +0500

    second commit

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,8 @@
+
+const Home = () => {
+  return (
+    <div className="text-xl underline" >Home</div>
+  )
+}
+
+export default Home
---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+
+
+
+export const routes = createBrowserRouter([
+    {
+        path:"/",
+        element:<Home/>
+    }
+])
